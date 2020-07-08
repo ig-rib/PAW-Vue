11938977f3dd5529db43d70ec5a22a56c5b58d19
Ignacio Ribas <iribas@itba.edu.ar>
2020-07-08 19:37:34 -0300

    [Router] First setup

--- tpe_paw_front/src/App.vue
+++ tpe_paw_front/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <v-app>
-    <v-app-bar
+  <v-app id="app">
+    APP
+    <router-view></router-view>
+    <!-- <v-app-bar
       app
       color="primary"
       dark
@@ -35,24 +37,14 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar> -->
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld
-  },
-
   data: () => ({
     //
   })
--- tpe_paw_front/src/router/index.js
+++ tpe_paw_front/src/router/index.js
@@ -1,22 +1,41 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import PersistentNavigator from '@/components/PersistentNavigator.vue'
+import Home from '@/views/Home.vue'
+import Nothing from '@/components/Nothing.vue'
 
 Vue.use(VueRouter)
+
+const defaultLang = 'en'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/' + defaultLang + '/feed'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/:langauge',
+    name: 'nothing',
+    redirect: {
+      name: 'feed'
+    },
+    comopnent: Nothing,
+    children: [
+      {
+        path: '',
+        redirect: {
+          name: 'feed'
+        },
+        component: PersistentNavigator,
+        children: [
+          {
+            path: 'feed',
+            name: 'feed',
+            component: Home
+          }
+        ]
+      }
+    ]
   }
 ]
 
