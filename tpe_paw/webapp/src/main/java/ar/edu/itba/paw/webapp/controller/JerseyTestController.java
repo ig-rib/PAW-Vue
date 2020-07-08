a1a4bc5565817322c63227637a080bde17244447
Ignacio Ribas <iribas@itba.edu.ar>
2020-07-08 02:05:12 -0300

    [pom, web.xml] First simple request working!

--- tpe_paw/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/JerseyTestController.java
+++ tpe_paw/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/JerseyTestController.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.webapp.controller;
 
 
+import ar.edu.itba.paw.webapp.dto.LanguageDto;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
@@ -16,7 +17,13 @@ public class JerseyTestController {
     @GET
     @Produces(value = {MediaType.APPLICATION_JSON})
     public Response test() {
-        return Response.ok().build();
+        LanguageDto langDto = new LanguageDto();
+        langDto.setName("JAVA");
+        return Response
+                .status(Response.Status.ACCEPTED)
+                .header("Access-Control-Allow-Origin", "*")
+                .entity(langDto)
+                .build();
     }
 
 }
