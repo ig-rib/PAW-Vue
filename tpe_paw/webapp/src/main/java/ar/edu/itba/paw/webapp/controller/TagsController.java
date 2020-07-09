c62d2bfab65fc109560ea3d4de34a1523b2d75a7
Ignacio Ribas <iribas@itba.edu.ar>
2020-07-09 20:00:11 -0300

    [Tags] Created endpoint for getting snippets for tagId

--- tpe_paw/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/TagsController.java
+++ tpe_paw/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/TagsController.java
@@ -1,6 +1,8 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.interfaces.service.SnippetService;
 import ar.edu.itba.paw.interfaces.service.TagService;
+import ar.edu.itba.paw.webapp.dto.SnippetDto;
 import ar.edu.itba.paw.webapp.dto.TagDto;
 import ar.edu.itba.paw.webapp.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import javax.ws.rs.core.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static ar.edu.itba.paw.webapp.utility.Constants.SNIPPET_PAGE_SIZE;
 import static ar.edu.itba.paw.webapp.utility.Constants.TAG_PAGE_SIZE;
 
 @Path("/tags")
@@ -17,6 +20,8 @@ public class TagsController {
 
     @Autowired
     private TagService tagService;
+    @Autowired
+    private SnippetService snippetService;
 
     @Context
     private UriInfo uriInfo;
@@ -29,7 +34,7 @@ public class TagsController {
 
         final List<TagDto> tags = tagService.getAllTags(true, false, null, page, TAG_PAGE_SIZE)
                 .stream()
-                .map(tag -> TagDto.fromTag(tag))
+                .map(TagDto::fromTag)
                 .collect(Collectors.toList());
 
         int tagsCount = tagService.getAllTagsCount(true, false, null);
@@ -44,6 +49,31 @@ public class TagsController {
         if (page < pageCount)
             respBuilder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page+1).build(), "next");
 
+        return respBuilder.build();
+    }
+
+    @GET
+    @Path("/{tagId}")
+    @Produces(value = {MediaType.APPLICATION_JSON})
+    public Response showSnippetsForTag(@QueryParam("page") @DefaultValue("1") int page,
+        @PathParam(value="tagId") long tagId) {
+
+        final List<SnippetDto> snippets = snippetService.findSnippetsForTag(tagId, page, SNIPPET_PAGE_SIZE)
+                .stream()
+                .map(SnippetDto::fromSnippet)
+                .collect(Collectors.toList());
+
+        int snippetsCount = snippetService.getAllSnippetsByTagCount(tagId);
+        int pageCount = (snippetsCount/SNIPPET_PAGE_SIZE) + ((snippetsCount % SNIPPET_PAGE_SIZE == 0) ? 0 : 1);
+
+        Response.ResponseBuilder respBuilder = Response.ok(new GenericEntity<List<SnippetDto>>(snippets) {})
+                .header("Access-Control-Allow-Origin", "*")
+                .link(uriInfo.getAbsolutePathBuilder().queryParam("page", 1).build(), "first")
+                .link(uriInfo.getAbsolutePathBuilder().queryParam("page",pageCount).build(), "last");
+        if (page > 1)
+            respBuilder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page-1).build(), "prev");
+        if (page < pageCount)
+            respBuilder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page+1).build(), "next");
         return respBuilder.build();
     }
 }
