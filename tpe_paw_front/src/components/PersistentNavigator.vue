98bd329b988e3ed14ace75955e3f16edfe6e29d1
Ignacio Ribas <iribas@itba.edu.ar>
2020-07-09 01:19:41 -0300

    [Router, Navigation] Added appbar and navigation drawer with working links to pages

--- tpe_paw_front/src/components/PersistentNavigator.vue
+++ tpe_paw_front/src/components/PersistentNavigator.vue
@@ -1,12 +1,84 @@
 <template>
   <div>
-    <h1>persistent navigator</h1>
-    <router-view></router-view>
+    <v-app-bar
+      absolute
+      color="grey"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-app-bar-nav-icon @click="navDrawer = !navDrawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer
+      fixed
+      v-model="navDrawer">
+       <v-list dense class="pt-0">
+      <v-list-item
+        v-for="item in paths"
+        :key="item.title"
+        :to="item.path"
+      >
+        <v-layout>
+          <v-flex>
+              <v-icon>{{ item.icon }}</v-icon>
+          </v-flex>
+
+          <v-flex>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-flex>
+        </v-layout>
+      </v-list-item>
+    </v-list>
+    </v-navigation-drawer>
+    <div>
+      <h1>persistent navigator</h1>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      navDrawer: true,
+      paths: [
+        {
+          title: this.$t('explore.title'),
+          icon: 'mdi-heart',
+          path: {
+            name: 'explore'
+          }
+        },
+        {
+          title: this.$t('languages.title'),
+          icon: 'mdi-magnify',
+          path: {
+            name: 'languages'
+          }
+        }
+      ]
+    }
+  },
+  methods: {
+    goto (path) {
+      this.$router.push(path)
+    }
+  }
 }
 </script>
