93f68f2e8dcd481cfe2de310c31ed6f78fea9f31
ahenestrosa <ahenestrosa@itba.edu.ar>
2020-07-11 15:52:28 -0300

    [Front] Snippet card component

---
+++ tpe_paw_front/src/components/Snippet.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container> 
+      <v-card max-width="420">
+          <!-- User and language -->
+          <v-container fill-height fluid>
+            
+              <v-row class="pt-0" align="start" justify="space-between">
+                  <v-col class="pt-0" cols="8">
+                    <v-list-item two-line>
+                      <v-avatar class="mr-2" color="indigo">
+                          <v-icon dark>mdi-account-circle</v-icon>
+                      </v-avatar>
+                      <v-list-item-content> 
+                        <v-list-item-title class="headline mb-1">{{ owner }}</v-list-item-title>
+                        <v-list-item-subtitle>{{ date }}</v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-col>
+
+                  <v-col cols='4'>
+                    <v-icon class="mr-1 mt-1">
+                      mdi-heart
+                    </v-icon>
+                    <v-chip
+                        class="mt-2"
+                        color="light-blue"
+                        label
+                        text-color="white"
+                    >
+                        {{ language }}
+                    </v-chip>
+                  </v-col>
+              </v-row>
+
+              <!-- Title and description -->
+              <v-list-item two-line>
+                <v-list-item-content> 
+                  <v-list-item-title class="headline mb-1">{{ title }}</v-list-item-title>
+                  <v-list-item-subtitle class="ml-1">{{ description }}</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+
+              <!-- Code preview -->
+              <v-textarea
+                class="pl-2"
+                name="input-7-1"
+                filled
+                label="Code preview"
+                :value="code"
+              ></v-textarea>
+
+          </v-container>
+      </v-card>
+  </v-container>
+  
+</template>
+
+<script>
+export default {
+  name: 'SnippetComponent', 
+
+  components: {
+    // TagComponent
+  },
+  props: {
+      title: String,
+      description: String,
+      code: String,
+      language: String,
+      favorite: Boolean,
+      date: Date,
+      owner: String
+  },
+
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- tpe_paw_front/src/views/languages/LanguagesSnippet.vue
+++ tpe_paw_front/src/views/languages/LanguagesSnippet.vue
@@ -1,11 +1,35 @@
 <template>
   <v-container>
     <div>
+        <SnippetComponent 
+            title='Titulo'
+            description='Descripcion'
+            code='Codigo'
+            language='lenguaje'
+            owner='Augusto'
+        />
+        <SnippetComponent/>
     </div>
   </v-container>
 </template>
 
 <script>
+import SnippetComponent from '@//components/Snippet.vue'
+
+export default {
+    name: 'languagesSnippet',
+    components: {
+        SnippetComponent
+    },
+    data () {
+    return {
+      snippetData: {
+        title: 'hola'
+      }
+    }
+  },
+
+}
 
 </script>
 
