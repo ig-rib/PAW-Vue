6fa7ce03d84652dafb340baa9b485fe80cfdc4ad
Ignacio Ribas <iribas@itba.edu.ar>
2020-12-02 12:16:17 -0300

    [Tests] Done with tests for validations

--- tpe_paw_front/src/functions/validations.js
+++ tpe_paw_front/src/functions/validations.js
@@ -3,14 +3,14 @@ import i18n from '@/i18n'
 const validPassword = (password) => password.length >= 8 || i18n.t('validations.invalidPassword')
 const email = (wouldBeEmail) => {
    const regex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>().,;\s@"]+\.{1})+([^<>().,;:\s@"]{2,}))$/
-    return regex.test(wouldBeEmail) || i18n.t('validations.invalidEmail')
+    return (wouldBeMail != null && regex.test(wouldBeEmail)) || i18n.t('validations.invalidEmail')
   }
 const maxLength = (text, length) => (text != null && text.length <= length) || i18n.t('validations.maxLength', { maxLength: length })
 const minLength = (text, length) => (text != null && text.length <= length) || i18n.t('validations.minLength', { minLength: length })
 const lengthBetween = (text, minLength, maxLength) => (text.length >= minLength && text.length <= maxLength) || i18n.t('validations.lengthBetween', { minLength, maxLength })
-const usernamePattern = (text) => /^[a-zA-Z0-9-_.]+$/.test(text) || i18n.t('validations.usernamePattern')
+const usernamePattern = (text) => (text != null && /^[a-zA-Z0-9-_\.]+$/.test(text)) || i18n.t('validations.usernamePattern')
 const notBlankWithSpaces = (text) => (text != null && text.trim().length > 0) || i18n.t('validations.notBlankWithSpaces')
-const codePattern = (code) => /^[0-9]{6}$/.test(code) || i18n.t('validations.codePattern')
+const codePattern = (code) => (code != null && /^[0-9]{6}$/.test(code)) || i18n.t('validations.codePattern')
 
 export default {
     validPassword,
---
+++ tpe_paw_front/tests/unit/validations/codes.spec.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai'
+import i18n from '@/i18n'
+import validations from '@/functions/validations'
+
+describe ('Number and text code validations', () => {
+  it('Code pattern', () => {
+    let invalidNotice = i18n.t('validations.codePattern')
+    expect(validations.codePattern('123412')).to.be.equal(true)
+    expect(validations.codePattern('ad')).to.be.equal(invalidNotice)
+    expect(validations.codePattern('')).to.be.equal(invalidNotice)
+    expect(validations.codePattern('1234122')).to.be.equal(invalidNotice)
+    expect(validations.codePattern(null)).to.be.equal(invalidNotice)
+  })
+  it('Not blank with spaces', () => {
+    let invalidNotice = i18n.t('validations.notBlankWithSpaces')
+    expect(validations.notBlankWithSpaces('              ')).to.be.equal(invalidNotice)
+    expect(validations.notBlankWithSpaces('\t\n ')).to.be.equal(invalidNotice)
+    expect(validations.notBlankWithSpaces('someCode')).to.be.equal(true)
+    expect(validations.notBlankWithSpaces(null)).to.be.equal(invalidNotice)
+  })
+})
