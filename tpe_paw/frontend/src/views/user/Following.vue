2dc6265b6b07a399dee57fa9938083062402bb70
Ignacio Ribas <iribas@itba.edu.ar>
2020-12-12 01:15:51 -0300

    [Tags] Added button to go to followed tags

--- tpe_paw/frontend/src/views/user/Following.vue
+++ tpe_paw/frontend/src/views/user/Following.vue
@@ -5,6 +5,11 @@
         {{ $t('user.following.title') }}
       </v-flex>
     </v-layout>
+    <v-layout justify-center>
+      <v-btn @click="goToFollowedTags">
+        {{ $t('user.following.manageFollowedTags') }}
+      </v-btn>
+    </v-layout>
     <v-layout>
       <snippet-grid ref="userFavoritesSnippetGrid"></snippet-grid>
     </v-layout>
@@ -16,6 +21,16 @@ export default {
   title: 'Snippit - Following',
   mounted () {
     this.$on('searchResults', r => this.$refs.userFavoritesSnippetGrid.$emit('searchResults', r))
+  },
+  methods: {
+    goToFollowedTags () {
+      this.$router.push({
+        name: 'tags-main',
+        query: {
+          showOnlyFollowing: true
+        }
+      })
+    }
   }
 }
 </script>
